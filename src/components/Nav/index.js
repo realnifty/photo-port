53c0ae4be6e13868245d1733d9def94851ab1c8c
gordo <rogelioalbertopedraza@gmail.com>
2022-07-17 21:06:38 -0500

    conditional rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,13 @@ import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-  const { categories = [], setCurrentCategory, currentCategory, contactSelected, setContactSelected } = props;
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+    contactSelected,
+    setContactSelected,
+  } = props;
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
@@ -22,17 +28,22 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+            <a
+              data-testid="about"
+              href="#about"
+              onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span onClick={()=> setContactSelected(true)}>Contact</span>
+          <li className={`mx-2 ${contactSelected && "nav-active"}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && "navActive"
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                `navActive`
               }`}
               key={category.name}>
               <span
