311735e8dbc2b78a9cc16c0f1e28d55245262d78
realnifty <rogelioalbertopedraza@gmail.com>
2022-10-24 18:44:47 -0500

    Nav component test

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,15 +20,14 @@ function Nav() {
 	return (
 		<header>
 			<h2>
-				<a href="/">
-          <span role="img" aria-label="camera">📸</span>
-          Oh Snap!
+				<a data-testid="link" href="/">
+          <span role="img" aria-label="camera">📸</span>Oh Snap!
 				</a>
 			</h2>
 			<nav>
 				<ul className="flex-row">
 					<li className="mx-2">
-						<a href="#about">About me</a>
+						<a data-tesid="about" href="#about">About me</a>
 					</li>
 					<li>
 						<span>Contact</span>
