18597cea49e89b4c565b6678eda76687e6d371b7
realnifty <rogelioalbertopedraza@gmail.com>
2022-10-31 15:32:11 -0500

    contact form logic

--- src/App.js
+++ src/App.js
@@ -2,35 +2,39 @@ import { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+	const [categories] = useState([
+		{
+			name: 'commercial',
+			description:
+				'Photos of grocery stores, food trucks, and other commercial projects',
+		},
+		{ name: 'portraits', description: 'Portraits of people in my life' },
+		{ name: 'food', description: 'Delicious delicacies' },
+		{
+			name: 'landscape',
+			description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
+		},
+	]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+	const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-  return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-      ></Nav>
+	return (
+		<div>
+			<Nav
+				categories={categories}
+				setCurrentCategory={setCurrentCategory}
+				currentCategory={currentCategory}
+			></Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
-      </main>
-    </div>
-  );
+        <ContactForm></ContactForm>
+				<Gallery currentCategory={currentCategory}></Gallery>
+				<About></About>
+			</main>
+		</div>
+	);
 }
 
 export default App;
