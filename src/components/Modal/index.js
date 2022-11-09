af8cb5b2a6ade63800fadfb2c92e3b6f907cc4d0
realnifty <rogelioalbertopedraza@gmail.com>
2022-11-09 17:42:28 -0600

    modal close functionality

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-function Modal({ currentPhoto }) {
+function Modal({ onClose, currentPhoto }) {
   
   const { name, category, description, index } = currentPhoto;
 
@@ -11,7 +11,7 @@ function Modal({ currentPhoto }) {
 					alt="current category"
 				/>
 				<p>{description}</p>
-				<button type="button">Close this modal</button>
+				<button onClick={onClose} type="button">Close this modal</button>
 			</div>
 		</div>
 	);
